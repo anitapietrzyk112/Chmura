40eb5d1e66b36126f48e84d89951d38eaab4acd1
s-301-22 <s-301-22@gumiforest.uek.krakow.pl>
2018-05-26 14:35:37 +0200

    Add working version

--- media/s3_storage.py
+++ media/s3_storage.py
@@ -4,13 +4,13 @@ class S3MediaStorage:
  
   def __init__(self, s3, bucket_name):
     self.s3 = s3
-    self.bucket_name = bucket_name    
+    self.bucket_name = bucket_name
+    self.bucket = s3.Bucket(bucket_name)
 
-  def save(self,path,file_to_be_uploaded):
-    bucket = self.s3.Bucket(self.bucket_name)
-    bucket.put_object(Key=path, Body=file_to_be_uploaded)
+  def store(self, dest, source):
+    self.bucket.put_object(Key=dest, Body=source)
 
-  def contains(self,path):
+  def contains(self, path):
     try:
       self.s3.Object(self.bucket_name, path).load()
       return True
--- test_upload.py
+++ test_upload.py
@@ -1,14 +1,9 @@
 import boto3
 
-bucket_name = '204240-legutek'
+bucket_name = ''
 s3 = boto3.resource('s3')
 bucket = s3.Bucket(bucket_name)
 
-my_file = open('to_be_uploaded.txt' , 'w+')
-my_file.write('Here will be my test content')
-my_file.close
+my_file = open('my_hello.txt', 'rb')
+bucket.put_object(Key='omega/gamma/alfa/hello.txt', Body=my_file)
 
-bucket.put_object(
-    Key='omega/gammatest.txt', 
-    Body=open('to_be_uploaded.txt', 'rb')
-)
